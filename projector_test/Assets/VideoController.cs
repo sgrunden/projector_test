c8d48e1c7577ef85807b4f4cf026ae65ad904958
Savannah Grunden <savannah.grunden@gmail.com>
2019-08-23 14:46:03 -0700

    added drop down menu functionality for videoplayer

---
+++ projector_test/Assets/VideoController.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.Video;
+using UnityEngine.EventSystems;
+
+public class VideoController : MonoBehaviour
+{
+    public TMPro.TMP_Dropdown dropdown;
+    List<string> videoNames = new List<string>() { "Choose a Video:"};
+    
+    public VideoPlayer vp;
+    public VideoClip[] videos;
+
+    private string videoName ="";
+
+    public void Dropdown_IndexChanged(int index)
+    {
+        videoName = videoNames[index];
+        foreach (VideoClip v in videos)
+        {
+            if (videoName == v.name)
+            {
+                vp.clip = v;
+            }
+        }
+    }
+
+    void Start()
+    {
+        PopulateVideoList();
+    }
+
+    void PopulateVideoList()
+    {
+        foreach (VideoClip v in videos)
+        {
+            videoNames.Add(v.name);
+        }
+        dropdown.AddOptions(videoNames);
+    }
+
+}
