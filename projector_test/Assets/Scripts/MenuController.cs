9af93e9ce1deb99a1094c69dd37a9b962f25f5ba
Savannah Grunden <savannah.grunden@gmail.com>
2019-09-05 17:44:13 -0700

    Split VideoController script into VideoController and MenuController

---
+++ projector_test/Assets/Scripts/MenuController.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.Video;
+
+public class MenuController : MonoBehaviour
+{
+    public VideoController vc;
+
+    public Button play, pause, end;
+
+    public TMPro.TMP_Dropdown dropdown;
+    List<string> videoNames = new List<string>() { "Choose a Video:" };
+
+    private string videoName = "";
+
+    void Start()
+    {
+        PopulateVideoList();
+        play.onClick.AddListener(vc.PlayVideo);
+        pause.onClick.AddListener(vc.PauseVideo);
+        end.onClick.AddListener(vc.EndVideo);
+    }
+
+    void PopulateVideoList() //Takes the array of VideoClips from the VideoController script and adds the name of each clip to the drop down list.
+    {
+        foreach (VideoClip v in vc.videos)
+        {
+            videoNames.Add(v.name);
+        }
+        dropdown.AddOptions(videoNames);
+    }
+
+    void Dropdown_IndexChanged(int index) //Changes the clip to be played according to the drop down menu selection
+    {
+        videoName = videoNames[index];
+        foreach (VideoClip v in vc.videos)
+        {
+            if (videoName == v.name)
+            {
+                vc.vp.clip = v;
+                vc.vp.SetTargetAudioSource(0, vc.vpAudio); //Sets the VideoPlayer audio source to the AudioSource gameobject
+            }
+        }
+    }
+}
